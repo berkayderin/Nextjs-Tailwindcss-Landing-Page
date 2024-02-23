b355782c9543b259f732a23efacd3cd761768436
Berkay Derin <zewodi@gmail.com>
2024-02-24 00:16:56 +0300

    navbar end

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,33 +1,37 @@
+import logo from '../../assets/img/logo.png'
+
 const Navbar = () => {
 	return (
-		<div className="fixed top-3 left-1/2 transform -translate-x-1/2 z-[999] px-2 sm:px-6 lg:px-8">
-			<div className="flex flex-col sm:flex-row justify-center items-center gap-3 sm:gap-20 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-md border border-white border-opacity-40 rounded-2xl w-full sm:w-[52rem] h-[4.5rem] sm:h-[3.25rem] p-2 sm:p-5">
-				<div className="text-slate-600 font-bold text-base sm:text-lg">MelihBahri</div>
-				<ul className="text-gray flex gap-3 sm:gap-5 text-sm sm:text-base">
+		<div className="fixed top-0 left-0 z-[999] px-4 w-full mt-5">
+			<div className="flex flex-col md:flex-row justify-between items-center gap-4 md:gap-20 bg-cwhite bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-md border border-white border-opacity-40 rounded-2xl md:w-[60rem] h-[6rem] p-3 md:p-5 m-auto">
+				<div>
+					<img src={logo} alt="Logo" className="w-[132px] h-[32px]" />
+				</div>
+				<ul className="flex gap-4 md:gap-9 text-gray text-sm md:text-base items-center">
 					<li>
-						<a href="#" className="hover:text-gray-950 dark:hover:text-gray-300 transition">
+						<a href="#" className="hover:text-black transition-all duration-300 ease-in-out">
 							About
 						</a>
 					</li>
 					<li>
-						<a href="#" className="hover:text-gray-950 dark:hover:text-gray-300 transition">
+						<a href="#" className="hover:text-black transition-all duration-300 ease-in-out">
 							Recent Listings
 						</a>
 					</li>
 					<li>
-						<a href="#" className="hover:text-gray-950 dark:hover:text-gray-300 transition">
+						<a href="#" className="hover:text-black transition-all duration-300 ease-in-out">
 							Sold Games
 						</a>
 					</li>
 					<li>
-						<a href="#" className="hover:text-gray-950 dark:hover:text-gray-300 transition">
+						<a href="#" className="hover:text-black transition-all duration-300 ease-in-out">
 							Services
 						</a>
 					</li>
 				</ul>
-				<div className="flex gap-3 sm:gap-5 justify-center items-center text-sm sm:text-base">
-					<p className="text-gray">EN | TR</p>
-					<button className="text-white bg-blue shadow-sm shadow-blue rounded-lg p-1 sm:p-2">Contact</button>
+				<div className="hidden md:flex gap-3 items-center">
+					<p className="text-gray mr-2">EN | TR</p>
+					<button className="bg-blue text-white rounded-lg px-3 py-1 md:px-5 md:py-2">Contact</button>
 				</div>
 			</div>
 		</div>
